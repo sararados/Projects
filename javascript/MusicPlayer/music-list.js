e9b10401c5b61470f92193d6b574960f0a4ae355
Sara Rados <sararados@hotmail.com>
2023-03-12 23:18:09 +0100

    testing

---
+++ javascript/MusicPlayer/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+      name: "Harley Bird - Home",
+      artist: "Jordan Schor",
+      img: "music-1",
+      src:"music-1",  
+    },
+    {
+      name: "Ikson Anywhere - Ikson",
+      artist: " Audio Library",
+      img: "music-2",
+      src:"music-2",  
+    },
+    {
+      name: "Beuz & Jvna - Crazy",
+      artist: " Beuz & Jvna",
+      img: "music-3",
+      src:"music-3",  
+    },
+    {
+      name: "",
+      artist: "",
+      img: "music-4",
+      src:"music-4",  
+    },
+    {
+      name: "",
+      artist: "",
+      img: "music-5",
+      src:"music-5",  
+    },
+    {
+      name: "",
+      artist: "",
+      img: "music-6",
+      src:"music-6",  
+    },
+]
